f3a1b0b67536a875bff43339c18f2c15664fe46d
Shreyash Srivastava <shreyash.sri09@gmail.com>
2026-08-08 15:47:21 +0530

    chore: setup Next.js basePath routing for Cloudflare workers
    Signed-off-by: Shreyash Srivastava <shreyash.sri09@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/hustle',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/hustle',
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
